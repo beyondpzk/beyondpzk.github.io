f02bc64144999ea1acf3f7a34b0d4ad93036601f
beyondpzk <35320670+beyondpzk@users.noreply.github.com>
2026-02-07 00:48:52 +0000

    Deploying to gh-pages from @ beyondpzk/beyondpzk.github.io@ae1ff1989fda3794fa4f0bc51771646b0b105f3d 🚀

--- push.sh
+++ push.sh
@@ -1,8 +1,8 @@
-
+messages=$1
 today=$(date "+%Y-%m-%d %H:%M:%S")
 echo "当前日期: $today"
 npx prettier . --check
 npx prettier --write .
 git add .
-git commit -m "${today}"
-git push origin main
+git commit -m "${today}: ${messages}"
+git push -f origin main
