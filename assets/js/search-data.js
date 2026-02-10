717a1c955f8eba76f83a2f5c8ff91b9904cb1e73
beyondpzk <35320670+beyondpzk@users.noreply.github.com>
2026-02-10 06:30:57 +0000

    Deploying to gh-pages from @ beyondpzk/beyondpzk.github.io@1029e2c805b722b593bcbda370881a5d294853e0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,6 +94,17 @@ ninja.data = [{
             window.location.href = "/blog/2026/C_RADIOv4/";
           
         },
+      },{id: "post-gero",
+        
+          title: "GeRo",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/GeRo/";
+          
+        },
       },{id: "post-vlm4vla",
         
           title: "VLM4VLA",
@@ -136,6 +147,28 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2025/SurveyOnWorldModelsForEmbodiedAI/";
+          
+        },
+      },{id: "post-vla-embodedai",
+        
+          title: "VLA_embodedAI",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/VLA_embodedAI/";
+          
+        },
+      },{id: "post-autovla",
+        
+          title: "AutoVLA",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/AutoVLA/";
           
         },
       },{id: "post-meanflows",
@@ -347,6 +380,17 @@ ninja.data = [{
             window.location.href = "/blog/2023/Sparse4DV2/";
           
         },
+      },{id: "post-act",
+        
+          title: "ACT",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/ACT/";
+          
+        },
       },{id: "post-streampetr",
         
           title: "StreamPETR",
@@ -446,6 +490,28 @@ ninja.data = [{
             window.location.href = "/blog/2022/PETRV2/";
           
         },
+      },{id: "post-mu-law",
+        
+          title: "mu_law",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2022/mu_law/";
+          
+        },
+      },{id: "post-gato",
+        
+          title: "Gato",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2022/Gato/";
+          
+        },
       },{id: "post-displaying-external-posts-on-your-al-folio-blog",
         
           title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
@@ -543,6 +609,17 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2020/TNT/";
+          
+        },
+      },{id: "post-cvae-vs-vae",
+        
+          title: "CVAE_VS_VAE",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2020/CVAE_VS_VAE/";
           
         },
       },{id: "post-detr",
